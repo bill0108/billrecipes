d76ecf4d55a128b218a95306ad6c4d6be5e04c2d
bill0108 <fhubil27@gmail.com>
2021-05-04 13:49:40 +0800

    latest

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,59 @@
+class TodosController < ApplicationController
+ before_action :set_todo, only:[:show,:edit,:update,:destroy]
+	def new
+	@todo = Todo.new
+	end
+	
+	def create
+	@todo = Todo.new(todo_params)
+	if@todo.save
+	flash[:notice]= "todo was created successfully"
+	redirect_to todo_path(@todo)
+
+	 else 
+	 render 'new'
+         end 
+	end
+
+	def show
+	end
+
+
+        def edit 
+	end
+
+	def update
+	@todo=Todo.find(params[:id])
+	if @todo.update(todo_params)
+	flash[:notice]="todo was successfully updated"
+	redirect_to todo_path(@todo)
+
+	else
+	render 'edit'
+	end
+	end
+
+	def index 
+	@todos=Todo.all
+	end
+
+	def destroy
+
+	@todo.destroy
+	flash[:notice]="todos was successfully deleted"
+	redirect_to todos_path
+	end
+
+  private
+	def set_todo
+	@todo=Todo.find(params[:id])
+	end
+
+	def todo_params
+	params.require(:todo).permit(:name,:description)
+	end
+	
+	
+	
+	
+end
